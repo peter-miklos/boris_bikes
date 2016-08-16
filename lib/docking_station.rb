a8e7ec84fb59a37007c62bc739231ed48de0b5fa
naomi <naomi.emma.spencer@gmail.com>
2016-08-16 16:32:34 +0100

    step 11

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,7 +1,18 @@
 require_relative "bike"
 
 class DockingStation
+  attr_reader :capacity_bikes, :available_bikes
+  # will assign 10 to the available_bikes
+  def initialize
+    @capacity_bikes = 10
+    @available_bikes = []
+  end
+  # will release a bike
   def release_bike
     Bike.new
+  end
+  # will dock a bike
+  def dock(bike)
+    @available_bikes.push(bike)
   end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -7,4 +7,19 @@ describe DockingStation do
   it "return true on checking the condition of the bike" do
     expect(DockingStation.new.release_bike.working?).to eq true
   end
+  it "responds to the dock method" do
+    expect(DockingStation.new.respond_to?("dock")).to eq true
+  end
+  it "what is the docking stations capacity" do
+    expect(DockingStation.new.capacity_bikes).to eq 10
+  end
+  it "is the docked bike available in the docking station" do
+    station = DockingStation.new
+    station.dock("")
+    expect(station.available_bikes).to eq [""]
+  end
+
+  it "How many bikes are available" do
+    expect(DockingStation.new.available_bikes).to eq []
+  end
 end
